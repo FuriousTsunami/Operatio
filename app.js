ac730d3405bee2a25201c1fc04f2b15a12c9f653
FuriousTsunami <69990380+FuriousTsunami@users.noreply.github.com>
2020-10-28 16:47:20 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register("/sw.js").then(function(registration) {
+      // Registration Successful
+      console.log("ServiceWorker Registered, Scope: ", registration.scope);
+    }, function(err) {
+      // Registration Failed
+      console.log("ServiceWorker Not Registered: ", err);
+    });
+  });
+}
