5fc66a0548709905ee12eb4fff2acb16dccd7aee
FuriousTsunami <69990380+FuriousTsunami@users.noreply.github.com>
2020-10-28 16:47:56 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const staticCacheName = "staticCacheV9";
+const assets = [
+  "/",
+  "/index.html",
+  "/CSS/style.css",
+  "/CSS/w3.css",
+  "/Images/Icon-72.png",
+  "/JavaScript/app.js",
+  "/JavaScript/ui.js",
+  "/About",
+  "/Explore",
+  "/Favorites",
+  "/GetStarted",
+  "/Settings",
+  "/SignIn",
+  "/manifest.json",
+];
+
+self.addEventListener("install", evt => {
+  console.log("ServiceWorker Installed");
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("Caching Assets");
+      for(var i = 0; i <= assets.length; i++){
+        cache.add(assets[i]);
+      }
+    })
+  );
+});
+self.addEventListener("activate", evt => {
+  console.log("ServiceWorker Activated");
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return fetch(fetchEvent.request) || res;
+    })
+  )
+})
