eae6898974991354d66d39dcafc80722be873caf
FuriousTsunami <69990380+FuriousTsunami@users.noreply.github.com>
2020-10-28 16:44:20 -0400

    Create ui.js

---
+++ JavaScript/ui.js
@@ -0,0 +1,10 @@
+// Function to open and close menu
+function openMenu() {
+  document.getElementById("menu").style.display = "block";
+  document.getElementById("menuOverlay").style.display = "block";
+}
+ 
+function closeMenu() {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("menuOverlay").style.display = "none";
+}
